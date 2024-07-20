1f7647e002ca1a3fc7601796578aec8f1d1eff4e
Ammar Hassan <homesuper7@gmail.com>
2024-07-20 08:37:55 +0500

    Added register buttons and animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,24 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="bg-cover flex flex-col justify-center items-center w-1/2 min-h-screen bg-[url('https://cdn.leonardo.ai/users/13ae5bda-727f-40cc-8ecc-9ea3f021a830/generations/8be1e685-6e90-493a-bbcf-7350afd2c781/Default_A_minimalist_overhead_shot_of_two_meticulously_chosen_0.jpg')]">
-        
+      <div className="bg-cover flex flex-col justify-center items-center w-1/2 min-h-screen relative">
+        <div className="absolute inset-0 bg-cover bg-center transition-opacity duration-300 ease-in-out hover:opacity-0 bg-[url('https://cdn.leonardo.ai/users/13ae5bda-727f-40cc-8ecc-9ea3f021a830/generations/8be1e685-6e90-493a-bbcf-7350afd2c781/Default_A_minimalist_overhead_shot_of_two_meticulously_chosen_0.jpg')]">
+        </div>
+        <div className="absolute inset-0 flex flex-col justify-center items-center transition-opacity duration-300 ease-in-out opacity-0 hover:opacity-100 bg-gradient-to-br from-sky-200 via-rose-300 to-slate-600">
+          {/* <form className="flex flex-col justify-center items-center">
+            <h1 className="text-7xl font-bold leading-snug mb-2 bg-gradient-to-r from-black via-neutral-700 to-gray-900 text-transparent bg-clip-text">Welcome!</h1>
+            <label htmlFor="name" className="self-start mt-2 font-normal text-lg bg-gradient-to-r from-black via-neutral-700 to-gray-900 text-transparent bg-clip-text">Enter name:</label>
+            <input type="text" className="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700/50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            <label htmlFor="email" className="self-start mt-2 font-normal text-lg bg-gradient-to-r from-black via-neutral-700 to-gray-900 text-transparent bg-clip-text">Enter email:</label>
+            <input type="text" className="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700/50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+          </form> */}
+
+          <h1 className="text-7xl font-bold leading-snug mb-2 bg-gradient-to-r from-black via-neutral-700 to-gray-900 text-transparent bg-clip-text">Welcome!</h1>
+          <div className="flex space-x-4">
+            <button className="text-xl rounded-lg py-2 px-5 bg-gradient-to-r from-neutral-900 to-neutral-800 transition-opacity duration-300 ease-in-out opacity-70 hover:opacity-100">Sign Up</button>
+            <button className="text-xl rounded-lg py-2 px-5 bg-gradient-to-r from-neutral-900 to-neutral-800 transition-opacity duration-300 ease-in-out opacity-70 hover:opacity-100">Log In</button>
+          </div>
+        </div>
       </div>
     </main>
   );
